0f97cab78a27182b424ee8729c94665404f1d36a
maxim <zanoga.maxim@gmail.com>
2022-11-10 16:45:34 -0800

    add modals to add, edit projects and urls

--- js/settings.js
+++ js/settings.js
@@ -12,3 +12,10 @@ function saveSettings() {
     localStorage.setItem("HomeLabSettings", settings);
 }
 
+function newProject() {
+    let projectName = document.getElementById("project-name").value;
+    console.log(projectName);
+    document.getElementById('projectsMenu').innerHTML += '<li><a class="dropdown-item" href="#">' + projectData["name"] + '</a></li>';
+}
+
+document.getElementById('create-project').addEventListener('click', newProject);
